28560dccff070deb873383957a7a94693a89c09b
Hansel Dunlop <hansel@interpretthis.org>
2015-11-27 16:03:31 +0000

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+= Mambu 
+
+Very alpha api package for Mambu. It's not advised for anyone outside of 
+Paze to use this until we improve it further as we'll be making large changes
+to the public API
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Mambu',
+    version='0.0.1',
+    description='Python module for Mambu API',
+    author='Paze.me Limited',
+    author_email='hansel.dunlop@paze.me',
+    url='https://www.mambu.com',
+    packages=['mambu'],
+)

01df43e31677148c54073ffe699c0569f1bc0234
Hansel Dunlop <hansel@interpretthis.org>
2015-11-30 11:53:14 +0000

    Include data directories

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Mambu',
-    version='0.0.2',
+    version='0.0.5',
     description='Python module for Mambu API',
     author='Paze.me Limited',
     author_email='hansel.dunlop@paze.me',
     url='https://www.mambu.com',
-    packages=['mambu'],
+    packages=['mambu', 'mambu.etc', 'mambu.tools'],
     install_requires=[
         'requests'
     ]

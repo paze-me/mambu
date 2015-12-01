9b7298894bc1b03490339fafc375ddd1ada64552
David Simpson <david.simpson@paze.me>
2015-12-01 13:06:07 +0000

    Moved methods on AbstractAPI into API
    in preparation for removal of util.py

--- mambu/api.py
+++ mambu/api.py
@@ -1,20 +1,52 @@
 import threading
+import logging
+import requests
+import json
+import datetime
+
 
 from loans import LoansAPI
 from attachments import AttachmentsAPI
-from util import AbstractAPI, AbstractDataObject
 from tools import datelib, data
-
+from exception import MambuAPIException
 
 client_metadata = data.load_yaml('clients.yaml')
 
 
-class API(AbstractAPI):
+class API(object):
     def __init__(self, config_):
-        super(API, self).__init__(config_=config_)
         self.config = config_
         self.Loans = LoansAPI(self)
         self.Attachments = AttachmentsAPI(self)
+        self.base_url = 'https://' + self.config.domain + '/api/'
+        self.json_encoder = RequestJSONEncoder()
+
+    def _request(self, method, url, params=None, data=None):
+        headers = {'Content-Type': 'application/json'} if data else {}
+        data_str = self.json_encoder.encode(data)
+        logging.debug("Body: " + data_str)
+        response = getattr(requests, method)(
+            self.base_url + url, headers=headers, params=params, data=data_str,
+            auth=(self.config.username, self.config.password))
+        if response.status_code != 200 and response.status_code != 201:
+            raise MambuAPIException("Error performing the request",
+                                    response.status_code, response.json())
+        return response.json()
+
+    def _get(self, url, params=None, data=None):
+        return self._request('get', url, params, data)
+
+    def _post(self, url, params=None, data=None):
+        return self._request('post', url, params, data)
+
+    def _patch(self, url, params=None, data=None):
+        return self._request('patch', url, params, data)
+
+    def _delete(self, url, params=None, data=None):
+        return self._request('delete', url, params, data)
+
+    def _postfix_url(self, *args):
+        return '/'.join([arg for arg in args if arg is not None])
 
     def get_client(self, client_id=None, params=None):
         """Get the details for the client fro mambu
@@ -509,3 +541,27 @@ class API(AbstractAPI):
 
     class ClientIdDocument(AbstractDataObject):
         fields = client_metadata['id_document']
+
+
+class AbstractDataObject(object):
+    def __init__(self, **kw):
+        for key, val in kw.items():
+            self.__setattr__(key, val)
+
+    def __setattr__(self, key, val):
+        if key not in type(self).fields:
+            raise ValueError(key + " is not an allowed field")
+        self.__dict__[key] = val
+
+    def __getattr__(self, key):
+        return self.__dict__[key]
+
+
+class RequestJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, AbstractDataObject):
+            return o.__dict__
+        if isinstance(o, (datetime.date, datetime.datetime)):
+            return o.isoformat()
+        return o
+

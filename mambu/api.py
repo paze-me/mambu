26a276c5e5345e67a13674b134c452bbaebb89fb
David Simpson <david.simpson@paze.me>
2015-12-01 09:25:47 +0000

    Changed API to derive from AbstractAPI
    Moved SavingsTransactionsAPI methods into API

--- mambu/api.py
+++ mambu/api.py
@@ -2,21 +2,19 @@ from clients import ClientsAPI
 from loans import LoansAPI
 from savings import SavingsAPI
 from loan_transaction import LoanTransactionsAPI
-from savings_transactions import SavingsTransactionsAPI
 from attachments import AttachmentsAPI
 from custom_fields import CustomFieldsAPI
 from util import AbstractAPI
 
 
-class API(object):
+class API(AbstractAPI):
     def __init__(self, config_):
+        super(API, self).__init__(config_)
         self.config = config_
-        self._abstract_api = AbstractAPI(config_)
         self.Clients = ClientsAPI(self)
         self.Loans = LoansAPI(self)
         self.Savings = SavingsAPI(self)
         self.LoanTransactions = LoanTransactionsAPI(self)
-        self.SavingsTransactions = SavingsTransactionsAPI(self)
         self.Attachments = AttachmentsAPI(self)
         self.CustomFields = CustomFieldsAPI(self)
 
@@ -29,18 +27,20 @@ class API(object):
     def get_saving(self, saving_id):
         return self.Savings.get(saving_id)
 
+    def get_savings_transactions(self, savings_id):
+        return self._get(self._url_savings_transactions(savings_id))
+
     def get_attachment(self, attachment_id=None):
         return self.Attachments.get(attachment_id)
 
     def get_loan_product(self, loan_product_id=None):
-        return self._abstract_api._get(
-            self._abstract_api._postfix_url('loan_products', loan_product_id))
+        return self._get(self._url_loan_products(loan_product_id))
 
     def get_loan_product_encoded_key(self, loan_product_id=None):
         loan_product = self.get_loan_product(loan_product_id)
         return loan_product['encodedKey']
 
-    def get_transactions(self):
+    def get_loan_transactions(self):
         pass
 
     def create_client(self, *args, **kwargs):
@@ -48,6 +48,10 @@ class API(object):
 
     def create_loan(self, *args, **kwargs):
         return self.Loans.create(*args, **kwargs)
+
+    def create_savings_transaction(self, savings_id, savings_transaction=None):
+        return self._post(self._url_savings_transactions(
+            savings_id, savings_transaction))
 
     def approve_loan(self, loan_id):
         return self.LoanTransactions.approve(loan_id)
@@ -57,3 +61,10 @@ class API(object):
 
     def disburse(self, loan_id):
         return self.LoanTransactions.disburse(loan_id)
+
+    def _url_savings_transactions(self, savings_id, transactions_id=None):
+        return self._postfix_url(
+            'savings', savings_id, 'transactions', transactions_id)
+
+    def _url_loan_products(self, loan_product_id=None):
+        return self._postfix_url('loan_products', loan_product_id)

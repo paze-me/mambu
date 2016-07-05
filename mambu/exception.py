af28620093c68b01aac46d9bc9ffe8483721bda1
Hansel Dunlop <hansel@interpretthis.org>
2016-07-05 11:03:18 +0100

    Prevent certain responses from breaking exception reporting

--- mambu/exception.py
+++ mambu/exception.py
@@ -1,13 +1,14 @@
 class MambuAPIException(Exception):
     def __init__(self, message, code, status):
+        self.return_code = status.get('returnCode', '')
+        self.return_status = status.get('returnStatus', '')
+        self.code = code
+
         msg = '{}, code: {}, return code: {}, return status: {}'.format(
-            message, code, status.get('returnCode', ''),
-            status.get('returnStatus', ''))
+            message, self.code, self.return_code, self.return_status,
+        )
         if 'errorSource' in status:
             msg += ', source: ' + str(status['errorSource'])
             self.error_source = status['errorSource']
-            
+
         super(MambuAPIException, self).__init__(msg)
-        self.code = code
-        self.return_code = status['returnCode']
-        self.return_status = status['returnStatus']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 
 setup(
     name='Mambu',
